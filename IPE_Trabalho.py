f27ce09ddba303df1a08529e4ff075731a00b869
dariocostta <dariocostta@gmail.com>
2019-10-25 00:23:08 -0300

    Adicionado Ocio e Espera, com modificação na exibição dos graficos

--- IPE_Trabalho.py
+++ IPE_Trabalho.py
@@ -14,6 +14,7 @@ display(HTML("<style>.container { width:95% !important;}</style>"))
 %config InlineBackend.figure_format='retina' '''
 
 import matplotlib.pyplot as plt
+import pandas as pd
 from random import gammavariate as G
 
 
@@ -29,10 +30,15 @@ while 1:
    a_alpha = float(input("\tAlpha:"))
    a_beta  = float(input("\tBeta:"))
 
+
+   # In[23]:
+   tamanho = 1000 #tamanho da tabela numero de linhas
+
+   
    # In[2]:
-   EntreChegadas = [G(c_alpha, c_beta) for i in range(1000)]
+   EntreChegadas = [G(c_alpha, c_beta) for i in range(tamanho)]
    # In[3]:
-   Atendimentos  = [G(a_alpha, a_beta) for i in range(1000)]
+   Atendimentos  = [G(a_alpha, a_beta) for i in range(tamanho)]
    # #### Dados secundarios
     
    # In[4]:    
@@ -40,7 +46,7 @@ while 1:
    soma=0
    for xi in EntreChegadas: 
        soma +=xi
-       Chegadas.append(soma)        
+       Chegadas.append(soma) 
     # In[5]:    
    IAtendimento = [] #Inicio do Atendimento
    FAtendimento = [] #Fim do Atendimento
@@ -48,20 +54,20 @@ while 1:
    IAtendimento.append(Chegadas[0])
    FAtendimento.append(IAtendimento[0]+Atendimentos[0])
     
-   for i in range( 1, 1000):
+   for i in range( 1, tamanho):
        IAtendimento.append(max(Chegadas[i], FAtendimento[i-1]))
-       FAtendimento.append(IAtendimento[i]+Atendimentos[i])    
+       FAtendimento.append(IAtendimento[i]+Atendimentos[i])
        
-    # ## Tamanho da Fila    
+    # ## Tamanho da Fila
     # In[6]:
    Ocorrencias = [[0 , 0]]
     
-   for i in range(1000):
+   for i in range(tamanho):
        Ocorrencias.append([Chegadas[i],  1])
        Ocorrencias.append([FAtendimento[i], -1])
    Ocorrencias = sorted(Ocorrencias)
 
-   TamanhoFila = [[0], [0]]
+   TamanhoFila = [[0], [0]] #TamanhoFila[0] = Instante de mudança | TamanhoFila[1] = Tamanho
    j=1
    for i in range(1, len(Ocorrencias)):
        TamanhoFila[0].append(Ocorrencias[i][0])
@@ -71,32 +77,59 @@ while 1:
        TamanhoFila[1].append(TamanhoFila[1][j-1]+Ocorrencias[i][1])
        j=j+1
     
-   # In[7]:
-   fig = plt.figure()
-   grafico1 = fig.add_subplot(211)
-   grafico2 = fig.add_subplot(212)
-   
-   grafico1.set_title("Informações Basicas")
-   grafico1.set_ylabel("Tempo (min.)")
-   grafico1.set_xlabel("Cliente")
-   grafico1.plot(EntreChegadas)
-   grafico1.plot(Atendimentos)
-   
-   grafico1.legend( ["Chegadas a="+str(c_alpha)+" b="+str(c_beta),"Atendimentos a="+str(a_alpha)+" b="+str(a_beta)], shadow=True)    
-   grafico1.grid(True)
+   # In[20]
+   Ocio = [Chegadas[0]]
+   for i in range( 1, tamanho):
+       Ocio.append(max(Chegadas[i]-FAtendimento[i-1], 0))
 
-   # ## Fila
+   Espera = []
+   for i in range(tamanho):
+       Espera.append(IAtendimento[i]-Chegadas[i])
     
-   inic=0
-   fim =4001 # Quantidade de ocorrencias = fim-inic
+    # # Graficos
+   # In[7]:
+   ''' 
+   fig = plt.figure()
+   grafico1 = fig.add_subplot(311)
+   grafico2 = fig.add_subplot(312)
+   grafico3 = fig.add_subplot(313)'''
    
-   grafico2.plot(TamanhoFila[0][inic:fim], TamanhoFila[1][inic:fim], "-bD")
-   grafico2.set_title("Tamanho da Fila")
-   grafico2.set_ylabel("Quantidade de pessoas")
-   grafico2.set_xlabel("Tempo (min.)")
-   grafico2.legend( ["Tamanho da Fila"], shadow=True)
-   grafico2.grid(True)
-   plt.show()
+   
+
+   while(1):
+       print("-----------Dados--------")
+       switch = int(input("\t1 - Tamanho da fila\n\t2 - Dados Basicos\n\t3 - Ocio e Espera\n\t0 - Sair\n"))
+       if(switch==1):
+       # ## Fila
+           plt.plot(TamanhoFila[0], TamanhoFila[1], "-bD")
+           plt.title("Tamanho da Fila")
+           plt.ylabel("Quantidade de pessoas")
+           plt.xlabel("Tempo (min.)")
+           plt.legend( ["Tamanho da Fila"], shadow=True)
+           plt.grid(True)
+           plt.show()
+       if(switch==2):
+           plt.title("Informações Basicas")
+           plt.ylabel("Tempo (min.)")
+           plt.xlabel("Cliente")
+           plt.plot(EntreChegadas)
+           plt.plot(Atendimentos)
+           plt.legend( ["Chegadas a="+str(c_alpha)+" b="+str(c_beta),"Atendimentos a="+str(a_alpha)+" b="+str(a_beta)], shadow=True)    
+           plt.grid(True)
+           plt.show()
+       if(switch==3):
+           plt.title("Informações Complementares")
+           plt.ylabel("Tempo (min.)")
+           plt.xlabel("Ocio ou espera do cliente")
+           plt.plot(Ocio, "--b")
+           plt.plot(Espera, "-r")
+           plt.legend( ["Ocio ","Espera"], shadow=True)   
+           plt.grid(True)
+           plt.show()
+       if(switch==0):
+           break
+   
+
    # In[16]:
    op = bool(int(input("Deseja repetir? \n\t1 - Sim\n\t0 - Não\n")))
    if(op==False):
